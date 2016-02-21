25b48d71a35435a9e66ed899ddf96171bff8b470
William Hood <williamhood@Williams-MacBook-Pro-2.local>
2016-02-21 15:10:26 -0500

    working on lightbox

--- app.js
+++ app.js
@@ -20,13 +20,13 @@ albumCoverPhotos(albums);
 
 //SELECTING PHOTOS ON HOMEPAGE
 
-var selectedPhotos = "";
+var selectedPhotoAlbums = "";
 $('.home-wrapper').children('ul').find('div').on('click', function (event) {
   event.preventDefault();
   $ ('.album-page').removeClass('inactive');
   $ ('.home-page').addClass('inactive');
-  selectedPhotos = ($(this).attr('id'));
-  showPhotoAlbums(selectedPhotos);
+  selectedPhotoAlbums = ($(this).attr('id'));
+  showPhotoAlbums(selectedPhotoAlbums);
 });
 
 //Pulls Album Photos
@@ -43,7 +43,7 @@ var grabPhotos = function (album_choice) {
   var showPhotoAlbums = function(albumPagePhotos) {
   var displayPhotosArr = "";
   _.each(grabPhotos(albumPagePhotos), function (el) {
-    displayPhotosArr += "<li>";
+    displayPhotosArr += "<li class = 'photo-list'>";
     displayPhotosArr += "<div class = 'album-image-picture-container'>";
     displayPhotosArr += "<img class= 'albumPhoto' src='" + el.photoFull + "' alt=''/>";
     displayPhotosArr += "<h4>" + el.photoName + "</h4>";
@@ -60,7 +60,7 @@ var grabPhotos = function (album_choice) {
 var navigationLinks = function (albumTitleData) {
   var albumTitleArr = "";
   albums.forEach (function(el){
-    albumTitleArr += "<li id = '" + el.albumRel + "'>";
+    albumTitleArr += "<li class='"+ el.photoRel + "' id = '" + el.albumRel + "'>";
     albumTitleArr += "<div class = 'navigation-link-container' id ='" + el.albumRel + "'>";
     albumTitleArr += "<h4>";
     albumTitleArr += el.albumTitle;
@@ -74,14 +74,43 @@ navigationLinks(albums);
 
 //2. Displays New Album that is chosen
 
-selectedPhotos = "";
+selectedPhotoAlbums = "";
 $('.navigation').children('li').on('click', function (event) {
   console.log ("Navigation is being clicked");
   event.preventDefault();
-  selectedPhotos = ($(this).attr('id'));
+  selectedPhotoAlbums = ($(this).attr('id'));
   $('.album-wrapper').not(this).find('li').hide();
-  var photoBlah = showPhotoAlbums(selectedPhotos);
+  var photoBlah = showPhotoAlbums(selectedPhotoAlbums);
 });
 
+//LIGHTBOX
+//1. Toggle to Lightbox Section
+var selectedPhotos = "";
+$('body').on("click", ".photo-list", function (event) {
+console.log("Photo click is working!");
+event.preventDefault();
+$ ('.lightbox-page').removeClass('inactive');
+$ ('.album-page').addClass('inactive');
+selectedPhotos = ($(this).attr('class'));
+showLightbox (selectedPhotos);
+});
 
+var filterLargePhotos = function (photo_choice) {
+  var emptyArr = albums.filter (function (item) {
+    return item.photoRel === photo_choice;
+  });
+  return emptyArr[0].photos;
+};
+
+
+var showLightbox = function (rel) {
+  varDisplayLightbox = "";
+  _.each(filterLargePhotos(albumPagePhotos), function (el) {
+  varDisplayLightbox += "<h1>" + el.photoName + "</h1>";
+  varDisplayLightbox += "<div class ='lightbox-image-container'>";
+  varDisplayLightbox += "<img class= 'lightboxPhoto' src='" + el.photoFull + "' alt=''/>";
+  varDisplayLightbox +="</div>";
+});
+$('.lightbox-page').append(displayPhotosArr);
+};
 });//end of doc ready
